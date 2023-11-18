a5d0af8fc9208cdd55b362fda336e0afc474eb59
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2023-11-17 23:18:03 -0300

    redenring responses as json data

---
+++ routes.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'json'
+
+
+get '/' do 
+  content_type :json
+
+  data = {
+    message: 'Hello world!'
+  }
+
+  JSON.generate(data)
+end
+
+get '/hello' do 
+  'Yeah! it works'
+end
+
+# get '/documentation' do
+#   render './public/views/home.html'
+# end
+
---
+++ server.rb
@@ -0,0 +1,5 @@
+require 'sinatra'
+require 'json'
+require './routes'
+
+
