a9adabc3c6bcf8801b14e742b1b99168d4d1b5cb
Rodrigo Galdino <galdinorodrigo553@gmail.com>
2023-11-28 15:04:32 +0000

    config app to dynamically receive env port from heroku

--- app.rb
+++ app.rb
@@ -8,6 +8,13 @@ require './routes'
 # require './views/index.erb'
 
 
+
+# APP CONFIGURATIONS
+
+# port config
+set :port, ENV['PORT'] || 4567
+
+
 # views configs
 set :views, File.join(File.dirname(__FILE__), 'views')
 set :views_folder, File.join(File.dirname(__FILE__), 'public')
